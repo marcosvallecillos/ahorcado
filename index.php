c199644c528936fc8ac910ae8a93ea794f1f28e1
Marcos Vallecillos Usagre <marcosvallecillosu@gmail.com>
2024-11-28 16:00:14 +0100

    realizando cambios

--- index.php
+++ index.php
@@ -40,12 +40,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['letra'])) {
 if ($_SESSION['letras_acertadas'] == $_SESSION['palabra']) {
     echo "¡Enhorabuena! Has ganado :) La palabra era: " . $_SESSION['palabra'] . "<br>";
     session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    echo '<a href="ganar.php">Jugar de nuevo</a>';
     exit();
 } elseif ($_SESSION['vidas'] <= 0) {
     echo "Lo siento, has perdido :( La palabra era: " . $_SESSION['palabra'] . "<br>";
     session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    echo '<a href="perder.php">Jugar de nuevo</a>';
     exit();
 }
 ?>
